06f3f6c7282fce9bf65d4ca65352e687688f91c0
jalondharavivek <vivek.jalondhara.23@gmail.com>
2023-04-27 19:49:20 +0530

    finance

--- src/pages/Transaction/Components/Addtransaction.js
+++ src/pages/Transaction/Components/Addtransaction.js
@@ -1,18 +1,161 @@
 import React from "react";
 import { useState } from "react";
 import "./addtransaction.css";
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import getBase64 from "get-base64";
 const AddTransaction = () => {
+  const navigate = useNavigate();
   const [addtransaction, setAddtransaction] = useState({
     transactiondate: "",
     monthyear: "",
     transactiontype: "",
     fromaccount: "",
     toaccount: "",
-    amout: "",
+    amount: "",
     receipt: "",
     notes: "",
   });
+  const backtransactionpage = () => {
+    navigate("/");
+  };
+  // console.log(addtransaction, "vivek add tra data");
+  // const [datastore, setDatastore] = useState([
+  //   {
+  //     transactiondate: "",
+  //     monthyear: "",
+  //     transactiontype: "",
+  //     fromaccount: "",
+  //     toaccount: "",
+  //     amount: "",
+  //     receiptfile: "",
+  //     notes: "",
+  //   },
+  // ]);
+  // console.log(datastore, "vivek datat store");
+  console.log(addtransaction, "first");
+  //   useEffect(() => {
+  //   var get = JSON.parse(localStorage.getItem("addtransaction") || "[]");
+  //   console.log(get, "loggg");
+  //   console.log(addtransaction, "second");
+  //   console.log();
+  //   get.push({ addtransaction });
+
+  //   // localStorage.setItem('Transaction', JSON.stringify(get));
+  //   localStorage.setItem("addtransaction", JSON.stringify(get));
+  //   console.log(localStorage, "third");
+  // }, [addtransaction]);
+
+  const [error, setErrors] = useState({});
+  // console.log(error, "vivek");
+  const handleInput = (event) => {
+    const transactionvalue = {
+      ...addtransaction,
+      [event.target.name]: event.target.value,
+    };
+    setAddtransaction(transactionvalue);
+    // console.log(setAddtransaction(transactionvalue), "transaction values");
+    // console.log(transactionvalue, "trans value");
+  };
+
+
+
+  const addtransactionsubmit = (event) => {
+    event.preventDefault();
+    const errors = validation(addtransaction);
+
+    if (Object.values(errors).length > 0) {
+      setErrors(validation(addtransaction));
+    } else {
+      setAddtransaction(addtransaction);
+      var get = JSON.parse(localStorage.getItem("addtransaction") || "[]");
+      console.log(get, "loggg");
+      console.log(get.transactiondate,"log value of tra date ");
+      console.log(addtransaction, "second");
+      console.log();
+      get.push(addtransaction);
+
+      // localStorage.setItem('Transaction', JSON.stringify(get));
+      localStorage.setItem("addtransaction", JSON.stringify(get));
+      navigate("/");
+    }
+  };
+  const getBase64 = (file) => {
+    return new Promise((resolve,reject) => {
+       const reader = new FileReader();
+       reader.onload = () => resolve(reader.result);
+       reader.onerror = error => reject(error);
+       reader.readAsDataURL(file);
+    });
+  } 
+  const fileupload = (e) => {
+    const file = e.target.files[0];
+    getBase64(file).then(base64 => {
+        const newobj = { ...addtransaction, receipt: base64 }
+        setAddtransaction(newobj)
+     console.log(base64,"base64");
+      console.debug("file stored",base64);
+    });
+};
+
+  const validation = (addtransaction) => {
+    const error = {};
+    // console.log(addtransaction.notes.length, "valueeeeee");
+    // console.log(addtransaction, "addtransaction values in validation");
+    // console.log(new Intl.NumberFormat("en-IN").format(addtransaction.amount));
+    const currentdate = new Date();
+
+    if (addtransaction.transactiondate === "") {
+      error.transactiondate = "Enter A Transaction Date";
+    } else if (addtransaction.transactiondate > currentdate.toISOString()) {
+      error.transactiondate = "Enter A Valid Transaction Date";
+    }
+
+    if (
+      addtransaction.monthyear === "" ||
+      addtransaction.monthyear === "Select Month Year"
+    ) {
+      error.monthyear = "Select Month Year";
+    }
+
+    if (
+      addtransaction.transactiontype === "" ||
+      addtransaction.transactiontype === "Select Transaction Type"
+    ) {
+      error.transactiontype = "Select Transaction Type";
+    }
+    if (
+      addtransaction.fromaccount === "" ||
+      addtransaction.fromaccount === "Select From Account"
+    ) {
+      error.fromaccount = "Select From Account";
+    }
+    if (
+      addtransaction.toaccount === "" ||
+      addtransaction.toaccount === "Select To Account"
+    ) {
+      error.toaccount = "Select To Account";
+    } else if (addtransaction.fromaccount === addtransaction.toaccount) {
+      error.toaccount = "Select To and From Diffrent Account";
+    }
+
+    if (addtransaction.amount.trim() === "") {
+      error.amount = "Enter A Amount";
+    } 
+    if (addtransaction.receipt === "") {
+      error.receipt = "Upload Receipt";
+    }
+    //  else if (!addtransaction.receipt.match(/(\.jpg|\.jpeg|\.png|\.gif)$/i)
+    // ) {
+    //   error.receipt = "Upload Only jpg/jpeg/png/gif file";
+    // }
+    if (addtransaction.notes.trim() === "") {
+      error.notes = "Enter A Notes";
+    } else if (addtransaction.notes.length >= 251) {
+      error.notes = "Notes less Than 250 char";
+    }
+    return error;
+  };
 
   return (
     <div>
@@ -21,94 +164,181 @@ const AddTransaction = () => {
           <span className="addtransactionheading">Add transaction</span>
         </div>
         <div className="formmain">
-          <form>
+          <form onSubmit={(e) => addtransactionsubmit(e)}>
             <div className="transactiondateclass">
               <div className="divpadding">
-                <label>Transaction Date : </label>
+                <label className="transactiondatelabel">
+                  Transaction Date :{" "}
+                </label>
               </div>
-              <div className="divpadding">
-                <input type="date" name="transactiondate"></input>
+              <div>
+                <input
+                  className="allinputbox"
+                  type="date"
+                  onInput={handleInput}
+                  name="transactiondate"
+                ></input>
+                <div>
+                  {error.transactiondate && (
+                    <p className="valicolor">{error.transactiondate}</p>
+                  )}
+                </div>
               </div>
 
               <div className="divpadding">
                 <label>Month Year :</label>
               </div>
-              <div className="divpadding">
-                <select>
-                  <option>Select Month Year</option>
-                  <option>Jan 2023</option>
-                  <option>feb 2023</option>
-                  <option>March 2023</option>
-                  <option>April 2034</option>
-                  <option>May 2023</option>
-                  <option>June 2023</option>
-                  <option>July 2023</option>
-                  <option>August 2023</option>
-                  <option>September 2023</option>
-                  <option>October 2023</option>
-                  <option>November 2023</option>
-                  <option>December 20232</option>
+              <div>
+                <select
+                  className="allinputbox"
+                  name="monthyear"
+                  onInput={handleInput}
+                >
+                  <option value="Select Month Year">Select Month Year</option>
+                  <option value="Jan 2023">Jan 2023</option>
+                  <option value="feb 2023">feb 2023</option>
+                  <option value="March 2023">March 2023</option>
+                  <option value="April 2023">April 2023</option>
+                  <option value="May 2023">May 2023</option>
+                  <option value="June 2023">June 2023</option>
+                  <option value="July 2023">July 2023</option>
+                  <option value="August 2023">August 2023</option>
+                  <option value="September 2023">September 2023</option>
+                  <option value="October 2023">October 2023</option>
+                  <option value="November 2023">November 2023</option>
+                  <option value="December 2023">December 20232</option>
                 </select>
+                <div>
+                  {error.monthyear && (
+                    <p className="valicolor">{error.monthyear}</p>
+                  )}
+                </div>
               </div>
               <div className="divpadding">
                 <label>Transaction Type : </label>
               </div>
-              <div className="divpadding">
-                <select>
-                  <option>Select Transaction Type</option>
-                  <option>Home Expense</option>
-                  <option>Personal Expense</option>
-                  <option>Income</option>
-                  
+              <div>
+                <select
+                  className="allinputbox"
+                  name="transactiontype"
+                  onInput={handleInput}
+                >
+                  <option value="Select Transaction Type">
+                    Select Transaction Type
+                  </option>
+                  <option value="Home Expense">Home Expense</option>
+                  <option value="Personal Expense">Personal Expense</option>
+                  <option value="Income">Income</option>
                 </select>
+                <div>
+                  {error.transactiontype && (
+                    <p className="valicolor">{error.transactiontype}</p>
+                  )}
+                </div>
               </div>
               <div className="divpadding">
                 <label>From Account :-</label>
               </div>
-              <div className="divpadding">
-                <select>
-                    <option>Select From Acccount</option>
-                    <option>From Account</option>
-                    <option>Personal Acccount</option>
-                    <option>Real Leaving </option>
-                    <option>My Dream Home</option>
-                    <option>Full Circle</option>
-                    <option>Core Realtors</option>
-                    <option>Big Block</option>
-                    
-
+              <div>
+                <select
+                  className="allinputbox"
+                  name="fromaccount"
+                  onInput={handleInput}
+                >
+                  <option value="Select From Account">
+                    Select From Account
+                  </option>
+                  <option value="Personal Acccount">Personal Acccount</option>
+                  <option value="Real Leaving ">Real Leaving </option>
+                  <option value="My Dream Home">My Dream Home</option>
+                  <option value="Full Circle">Full Circle</option>
+                  <option value="Core Realtors">Core Realtors</option>
+                  <option value="Big Block">Big Block</option>
                 </select>
+                <div>
+                  {error.fromaccount && (
+                    <p className="valicolor">{error.fromaccount}</p>
+                  )}
+                </div>
               </div>
               <div className="divpadding">
                 <label>To Account :-</label>
               </div>
-              <div className="divpadding">
-                <select>
-                    <option>Select From Acccount</option>
-                    <option>From Account</option>
-                    <option>Personal Acccount</option>
-                    <option>Real Leaving </option>
-                    <option>My Dream Home</option>
-                    <option>Full Circle</option>
-                    <option>Core Realtors</option>
-                    <option>Big Block</option>
-                
+              <div>
+                <select
+                  className="allinputbox"
+                  name="toaccount"
+                  onInput={handleInput}
+                >
+                  <option value="Select To Account">Select To Account</option>
+                  <option value="Personal Acccount">Personal Acccount</option>
+                  <option value="Real Leaving">Real Leaving</option>
+                  <option value="My Dream Home">My Dream Home</option>
+                  <option value="Full Circle">Full Circle</option>
+                  <option value="Core Realtors">Core Realtors</option>
+                  <option value="Big Block">Big Block</option>
                 </select>
+                <div>
+                  {error.toaccount && (
+                    <p className="valicolor">{error.toaccount}</p>
+                  )}
                 </div>
-                <div className="divpadding">
-                <label>Receipt :-</label>
-                </div>
-              <div className="divpadding">
-                <input type="file"></input>
               </div>
-              
+              <div className="divpadding">
+                <label>Amount :-</label>
+              </div>
+              <div>
+                <input
+                  placeholder="Amount"
+                  name="amount"
+                  onInput={handleInput}
+                  className="allinputbox"
+                  type="text"
+                ></input>
+                <div>
+                  {error.amount && <p className="valicolor">{error.amount}</p>}
+                </div>
+              </div>
+              <div className="divpadding">
+                <label>Receipt :-</label>
+              </div>
+              <div>
+                <input
+                  onChange={fileupload}
+                  className="allinputbox"
+                  type="file"
+                  name="receipt"
+                ></input>
+                <div>
+                  {error.receipt && (
+                    <p className="valicolor">{error.receipt}</p>
+                  )}
+                </div>
+              </div>
+
               <div className="divpadding">
                 <label>Note :-</label>
-                </div>
-              <div className="divpadding" >
-                <input type="text" placeholder="Add Note"></input>
               </div>
+              <div>
+                <input
+                  onInput={handleInput}
+                  className="allinputbox"
+                  type="text"
+                  name="notes"
+                  placeholder="Add Note"
+                ></input>
+                <div>
+                  {error.notes && <p className="valicolor">{error.notes}</p>}
+                </div>
+              </div>
+              <div>
+                <input className="addtransactionback1" type="submit"></input>
               
+                  <p className="addtransactionback" onClick={backtransactionpage}>
+                    Back
+                  </p>
+                
+              </div>
             </div>
           </form>
         </div>
--- src/pages/Transaction/Components/Financemain.js
+++ src/pages/Transaction/Components/Financemain.js
@@ -2,11 +2,108 @@ import React from "react";
 import { useState } from "react";
 import "./financemain.css";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 const Financetrackerform = () => {
+  const [alltransaction, setAlltransaction] = useState([]);
+
+  const [order, setOrder] = useState("default");
   const navigate = useNavigate();
+  const [grpby, setgrpby] = useState({});
+  const [data1, setData] = useState([]);
+  console.log(data1, "inittt");
   const addtransaction = () => {
     navigate("/addtransaction");
   };
+  console.log(data1, "array of object");
+  useEffect(() => {
+    setAlltransaction(
+      JSON.parse(localStorage.getItem("addtransaction") || "[]")
+    );
+  }, []);
+
+  // const datagrph = (event) => {
+  //   console.log(event,"vivek valyueeeee");
+  //   const transactionvalue = {
+  //     ...grpby,
+  //     [event.target.name]: event.target.value,
+  //   };
+  //   setgrpby(datagrp(transactionvalue));
+  // };
+
+  const datagrph = (val) => {
+    const p = val.target.value;
+    if(val === "None"){
+      const GROUPBY1 = [...alltransaction];
+      setData(GROUPBY1)
+    }else{
+    console.log("valll",p, "vvdvsvsvsvsv");
+    const GROUPBY = [...alltransaction];
+    console.log(GROUPBY, "valueeeeeee");
+    const key = {
+      monthyear: "monthyear",
+      transactiondate: "transactiondate",
+      transactiontype: "transactiontype",
+      amount: "amount",
+    };
+    let cats = GROUPBY.reduce((datagroupby, key) => {
+      {
+        console.log(key, "sgtgdgdgdg");
+      }
+      (datagroupby[key[p]] = datagroupby[key[p]] || []).push(key);
+
+      return datagroupby;
+    }, {});
+
+    setData(cats);
+    console.log(cats, "dgdg");
+    // console.log("heeeeee", cats);
+    // console.log(datagrp, "vviviviviv");
+  }
+  };
+
+  // console.log(datagrp, "onnnnn");
+
+  const sortdatacolumn = (col) => {
+    if (order === "default") {
+      console.log(col, "dtaaaaaa");
+      const sorted = [...alltransaction].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      console.log(sorted, "dtaaaaaa");
+      setAlltransaction(sorted);
+      setOrder("DSC");
+    } else if (order === "DSC") {
+      console.log(col, "dtaaaaaa");
+      const sorted = [...alltransaction].sort((a, b) =>
+        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      );
+      console.log(sorted, "dtaaaaaa");
+      setAlltransaction(sorted);
+      setOrder("default");
+    }
+    // } else if (order === "fordefault") {
+    //   console.log(col, "dtaaaaaa");
+    //   // const sorted = [...alltransaction]
+    //   // console.log(sorted, "dtaaaaaa");
+    //   setAlltransaction(
+    //     JSON.parse(localStorage.getItem("addtransaction") || "[]")
+    //   );
+    //   setOrder("default");
+    // }
+  };
+
+  const sortdatacolumn1 = (col) => {
+    if (order === "default") {
+
+      const sorted = [...data1].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      console.log(sorted, "dtaaaaasssa");
+      console.log(data1, "dtaaaaaa");
+      setAlltransaction(sorted);
+      setOrder("DSC");
+    }
+  }
 
   return (
     <div>
@@ -18,30 +115,161 @@ const Financetrackerform = () => {
           ADD +
         </p>
       </div>
-      <div className="Tablefinancem">
-        <table>
-          <tr>
-            <th>Transaction Date</th>
-            <th>Month Year</th>
-            <th>Transacton Type</th>
-            <th>From Account</th>
-            <th>To Acccount</th>
-            <th>Amount</th>
-            <th>Receipt</th>
-            <th>Notes</th>
-          </tr>
-          <tr>
-            <td></td>
-            <td>vsd</td>
-            <td>sdv</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </table>
+      <div>
+        <select name="groupbya" onInput={(event) => datagrph(event)}>
+          <option value="None">None</option>
+          <option value="monthyear">monthyear</option>
+          <option value="transactiondate">transactiondate</option>
+          <option value="amount">Amount</option>
+          <option value="transactiontype">transactiontype</option>
+        </select>
       </div>
+      {/* {alltransaction === [] ? ( */}
+        <div className="Tablefinancem">
+          <table>
+            <tr>
+              <th onClick={() => sortdatacolumn("transactiondate")}>
+                Transaction Date{" "}
+                {order === "default" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}{" "}
+              </th>
+              <th onClick={() => sortdatacolumn("monthyear")}>
+                Month Year{" "}
+                {order === "ASC" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}{" "}
+              </th>
+              <th onClick={() => sortdatacolumn("transactiontype")}>
+                Transacton Type{" "}
+                {order === "ASC" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}
+              </th>
+              <th onClick={() => sortdatacolumn("fromaccount")}>
+                From Account{" "}
+                {order === "ASC" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}
+              </th>
+              <th onClick={() => sortdatacolumn("toaccount")}>
+                To Acccount{" "}
+                {order === "ASC" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}
+              </th>
+              <th onClick={() => sortdatacolumn("amount")}>
+                Amount{" "}
+                {order === "ASC" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}
+              </th>
+              <th onClick={() => sortdatacolumn("receipt")}>
+                Receipt{" "}
+                {order === "ASC" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}
+              </th>
+              <th onClick={() => sortdatacolumn("notes")}>
+                Notes{" "}
+                {order === "ASC" ? (
+                  <i class="arrow up"></i>
+                ) : (
+                  <i class="arrow down"></i>
+                )}
+              </th>
+            </tr>
+
+            {alltransaction.map((addtransaction, index) => (
+              <tr key={index}>
+                <td>{addtransaction.transactiondate} </td>
+                <td>{addtransaction.monthyear}</td>
+                <td>{addtransaction.transactiontype}</td>
+                <td>{addtransaction.fromaccount}</td>
+                <td>{addtransaction.toaccount}</td>
+                <td>
+                  ₹{" "}
+                  {new Intl.NumberFormat("en-IN").format(addtransaction.amount)}
+                </td>
+                <td>
+                  <img src={addtransaction.receipt} className="imgwidth" />
+                </td>
+                <td>{addtransaction.notes}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      {/* ) : ( */}
+        <div>
+          <div className="Tablefinancem">
+            {/* {Object.keys(data1).map((item, index) =>
+            data1[item].map((groupbydata) => (
+              <table>
+                <tr key={index}>
+                  <th>Transaction Date</th>
+                  <th>Month year</th>
+                  <th>transactiontype</th>
+                  <th>fromaccount</th>
+                  <th>toaccount</th>
+                  <th>amount</th>
+                  <th>notes</th>
+                </tr>
+                <tr>
+                  <td>{groupbydata.transactiondate}</td>
+                  <td>{groupbydata.monthyear}</td>
+                  <td>{groupbydata.transactiontype}</td>
+                  <td>{groupbydata.fromaccount}</td>
+                  <td>{groupbydata.toaccount}</td>
+                  <td>{groupbydata.amount}</td>
+                  <td>{groupbydata.notes}</td>
+                </tr>
+              </table>
+            ))
+          )} */}
+            {Object.keys(data1).map((item, index) => (
+              <table className="vvv">
+                <tr className="itemd" key={index}>{item}</tr>
+                <tr key={index}>
+                  <th onClick={() => sortdatacolumn1("transactiondate")}> Transaction Date </th>
+                  <th>Month year</th>
+                  <th>transactiontype</th>
+                  <th>fromaccount</th>
+                  <th>toaccount</th>
+                  <th>amount</th>
+                  <th>notes</th>
+                </tr>
+
+                {data1[item].map((groupbydata, key) => (
+                  <tr>
+                    <td>{groupbydata.transactiondate}</td>
+                    <td>{groupbydata.monthyear}</td>
+                    <td>{groupbydata.transactiontype}</td>
+                    <td>{groupbydata.fromaccount}</td>
+                    <td>{groupbydata.toaccount}</td>
+                    <td> ₹{" "}
+                  {new Intl.NumberFormat("en-IN").format(groupbydata.amount)}</td>
+                    <td>{groupbydata.notes}</td>
+                  </tr>
+                ))}
+              </table>
+            ))}
+          </div>
+        </div>
+      {/* )} */}  
     </div>
   );
 };
